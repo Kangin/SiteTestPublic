780bbd7c8444d47a1d6be68a4690234005058f4a
Lucas Lee <jeohna@gmail.com>
2023-06-03 23:26:11 +0200

    Initial commit

---
+++ src/app/riot/riot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-riot',
+  templateUrl: './riot.component.html',
+  styleUrls: ['./riot.component.css']
+})
+export class RiotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
